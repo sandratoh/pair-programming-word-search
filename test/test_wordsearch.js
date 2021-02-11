66312834d2a973390c3af370329de88045a37390
Sandra To <sandra.to.e@gmail.com>
2021-02-11 01:01:32 +0000

    Function works with horizontal words

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -33,6 +33,18 @@ describe("#wordSearch()", function() {
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
 
+    it('should return true if the word is present vertically', () => {
+      const result = wordSearch([
+        ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+        ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+        ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+        ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+        ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+        ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+        ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+        ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+        ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ], 'LDL')
     assert.isTrue(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Partner Programming Activity
+// @sandratoh
+// @Isams01
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
+};
+
+const wordSearch = (letters, word) => {
+  const transposed = transpose(letters);
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalJoin = transposed.map(ls => ls.join(''));
+  for (t of verticalJoin) {
+    if (t.includes(word)) return true;
+  }
+
+  return false;
+};
+
+
+module.exports = wordSearch;
